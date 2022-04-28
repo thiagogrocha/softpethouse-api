bc97a60657f4cafb2fefd5536450c2745690b70d
TRochaDev <trochadev@gmail.com>
2022-04-27 23:59:04 -0300

    Cria Recursos.

---
+++ src/main/java/br/com/softpethouse/api/account/resource/AccountResource.java
@@ -0,0 +1,47 @@
+package br.com.softpethouse.api.account.resource;
+
+import br.com.softpethouse.api.account.dto.AccountDtoIn;
+import br.com.softpethouse.api.account.entity.AccountEntity;
+import br.com.softpethouse.api.account.service.AccountService;
+import br.com.softpethouse.api.commom.validation.ResponseError;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/accounts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class AccountResource {
+
+    private AccountService service;
+
+    @Inject
+    public AccountResource(AccountService service) {
+        this.service = service;
+    }
+
+    @POST
+    @Operation(summary = "Cria Conta de Usuário", description = "Cria Conta de Usuário")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "201", description = "Criado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = AccountEntity.class))),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response saveAccount(AccountDtoIn dto) {
+        try {
+            return service.save(dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
+}
---
+++ src/main/java/br/com/softpethouse/api/account/resource/BusinessResource.java
@@ -0,0 +1,4 @@
+package br.com.softpethouse.api.account.resource;
+
+public class BusinessResource {
+}
