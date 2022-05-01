fa89aae9afb2bf5d4636334ed6e3a617b5dc4a26
TRochaDev <trochadev@gmail.com>
2022-05-01 04:47:02 -0300

    Cria interface que contém os paths dos recursos.

---
+++ src/main/java/br/com/softpethouse/api/Resources.java
@@ -0,0 +1,16 @@
+package br.com.softpethouse.api;
+
+public interface Resources {
+
+    String ACCOUNT = "account";
+    String TYPEACCOUNT = "typeaccount";
+    String BUSINESS = "business";
+    String ADOPTION = "adoption";
+    String NEGOTIATION = "negotiation";
+    String PET = "pet";
+    String HISTORIC = "historic";
+    String PRODUCT = "product";
+    String USER = "user";
+
+
+}
