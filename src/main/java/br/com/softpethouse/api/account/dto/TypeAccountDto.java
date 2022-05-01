1a3667b7b413a49e8207fd5f82cdd860e84cb2cc
TRochaDev <trochadev@gmail.com>
2022-05-01 04:51:43 -0300

    Cria dto do tipo de conta.

--- src/main/java/br/com/softpethouse/api/account/dto/TypeAccountDto.java
+++ src/main/java/br/com/softpethouse/api/account/dto/TypeAccountDto.java
@@ -1,4 +1,23 @@
 package br.com.softpethouse.api.account.dto;
 
+import lombok.Data;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@Schema(name = "Type Account", description = "Type Account dto in")
 public class TypeAccountDto {
+
+    @NotNull(message = "O nome é obrigatório!")
+    @Schema(title = "Nome do Negócio")
+    @Parameter(description = "Nome parameter")
+    private String name;
+
+    @NotNull(message = "A descrição é obrigatória!")
+    @Schema(title = "Descrição")
+    @Parameter(description = "Descrição parameter")
+    private String description;
+
 }
