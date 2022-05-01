f8b65845642f248e653158a38ef2e4779892d301
TRochaDev <trochadev@gmail.com>
2022-05-01 04:48:09 -0300

    Cria dto de saída da Conta.

--- src/main/java/br/com/softpethouse/api/account/dto/AccountDtoOut.java
+++ src/main/java/br/com/softpethouse/api/account/dto/AccountDtoOut.java
@@ -1,4 +1,19 @@
 package br.com.softpethouse.api.account.dto;
 
+import br.com.softpethouse.api.user.dto.UserDto;
+import lombok.Data;
+
+@Data
 public class AccountDtoOut {
+
+    private UserDto user;
+
+    private TypeAccountDto typeAccount;
+
+    private BusinessDto business;
+
+    private String nickname;
+
+    private String email;
+
 }
