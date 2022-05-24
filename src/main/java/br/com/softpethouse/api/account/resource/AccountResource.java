cab7b1ebc50602b867ac066cb19792f240472d3f
TRochaDev <trochadev@gmail.com>
2022-05-24 00:17:36 -0300

    Corrige labels e cria recurso de atualização e de desabilitação.

--- src/main/java/br/com/softpethouse/api/account/resource/AccountResource.java
+++ src/main/java/br/com/softpethouse/api/account/resource/AccountResource.java
@@ -1,9 +1,9 @@
 package br.com.softpethouse.api.account.resource;
 
 import br.com.softpethouse.api.Resources;
-import br.com.softpethouse.api.account.dto.AccountDtoIn;
+import br.com.softpethouse.api.account.dto.AccountDtoCreate;
 import br.com.softpethouse.api.account.dto.AccountDtoOut;
-import br.com.softpethouse.api.account.entity.AccountEntity;
+import br.com.softpethouse.api.account.dto.AccountDtoUpdate;
 import br.com.softpethouse.api.account.service.AccountService;
 import br.com.softpethouse.api.commom.validation.ResponseError;
 import org.eclipse.microprofile.openapi.annotations.Operation;
@@ -30,20 +30,56 @@ public class AccountResource {
     }
 
     @POST
-    @Operation(summary = "Cria Conta de Usuário", description = "Cria Conta de Usuário")
+    @Operation(summary = "Criar", description = "Cria Conta de Usuário")
     @APIResponses(value = {
             @APIResponse(responseCode = "201", description = "Criado",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = AccountDtoOut.class))),
             @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
             @APIResponse(responseCode = "500", description = "Erro interno")})
-    public Response post(AccountDtoIn dto) {
+    public Response create(AccountDtoCreate dto) {
         try {
-            return service.save(dto);
+            return service.create(dto);
         } catch (Exception e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
         }
     }
 
+    @PUT
+    @Path("{accountId}")
+    @Operation(summary = "Atualizar", description = "Atualiza Conta de Usuário")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = String.class))),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response update(@PathParam("accountId") long id, AccountDtoUpdate dto) {
+        try {
+            return service.update(id, dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
+
+    @DELETE
+    @Path("{accountId}")
+    @Operation(summary = "Desativar", description = "Desativa Conta de Usuário")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = String.class))),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response disable(@PathParam("id") long id) {
+        try {
+            return service.disable(id);
+        }catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
 }
