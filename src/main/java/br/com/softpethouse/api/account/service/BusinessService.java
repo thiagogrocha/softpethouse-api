9904f5b4c05c8d4ea18ce7d9fa9629d4c58d8d1a
TRochaDev <trochadev@gmail.com>
2022-06-01 21:17:09 -0300

    Cria métodos create e update.

--- src/main/java/br/com/softpethouse/api/account/service/BusinessService.java
+++ src/main/java/br/com/softpethouse/api/account/service/BusinessService.java
@@ -1,9 +1,12 @@
 package br.com.softpethouse.api.account.service;
 
 import br.com.softpethouse.api.Resources;
+import br.com.softpethouse.api.account.dto.AccountDtoOut;
+import br.com.softpethouse.api.account.dto.AccountDtoUpdate;
 import br.com.softpethouse.api.business.dto.BusinessDto;
 import br.com.softpethouse.api.business.entity.BusinessEntity;
 import br.com.softpethouse.api.commom.validation.ResponseError;
+import br.com.softpethouse.api.commom.validation.ResponseMsg;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,6 +17,7 @@ import javax.validation.Validator;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Transactional
 @ApplicationScoped
@@ -26,7 +30,7 @@ public class BusinessService implements PanacheRepository<BusinessEntity> {
         this.validator = validator;
     }
 
-    public Response save(BusinessDto dto) {
+    public Response create(BusinessDto dto) {
         Set<ConstraintViolation<BusinessDto>> violations = validator.validate(dto);
 
         if (!violations.isEmpty())
@@ -40,4 +44,40 @@ public class BusinessService implements PanacheRepository<BusinessEntity> {
         return Response.created(UriBuilder.fromPath(Resources.BUSINESS).path(entity.getId().toString()).build()).build();
     }
 
+    public Response accounts() {
+        return Response.ok(findAll()
+                .stream()
+                .map(BusinessDto::fromEntity)
+                .collect(Collectors.toList())).build();
+    }
+
+    public Response update(long id, BusinessDto dto) {
+        BusinessEntity business = findById(id);
+
+        if (business == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Negócio não encontrado")).build();
+
+        Set<ConstraintViolation<BusinessDto>> violations = validator.validate(dto);
+
+        if (!violations.isEmpty())
+            return ResponseError
+                    .createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
+
+        business.setName(dto.getName());
+        business.setDescription(dto.getDescription());
+
+        return Response.status(Response.Status.OK.getStatusCode()).entity(new ResponseMsg("Negócio atualizado!")).build();
+    }
+
+    public Response disable(long id) {
+        BusinessEntity business = findById(id);
+
+        if (business == null)
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode()).entity(new ResponseMsg("Negócio não encontrado")).build();
+
+        business.setActive("N");
+
+        return Response.ok().entity(BusinessDto.fromEntity(business)).build();
+    }
 }
--- src/main/java/br/com/softpethouse/api/business/resource/BusinessResource.java
+++ src/main/java/br/com/softpethouse/api/business/resource/BusinessResource.java
@@ -1,6 +1,7 @@
 package br.com.softpethouse.api.business.resource;
 
 import br.com.softpethouse.api.Resources;
+import br.com.softpethouse.api.account.dto.AccountDtoUpdate;
 import br.com.softpethouse.api.business.dto.BusinessDto;
 import br.com.softpethouse.api.account.service.BusinessService;
 import br.com.softpethouse.api.commom.validation.ResponseError;
@@ -14,7 +15,6 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.stream.Collectors;
 
 @Path(Resources.BUSINESS)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -28,17 +28,15 @@ public class BusinessResource {
         this.service = service;
     }
 
-    @POST
-    @Operation(summary = "Cria Negócio", description = "Um Negócio pode ser um Petshop ou uma Clínica Veterinária")
+    @GET
+    @Operation(summary = "Negócios", description = "Lista Negócios, sejam Lojas ou Clínicas Veterinárias")
     @APIResponses(value = {
-            @APIResponse(responseCode = "201", description = "Criado",
+            @APIResponse(responseCode = "200", description = "Sucesso",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = BusinessDto.class))),
-            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
-                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
             @APIResponse(responseCode = "500", description = "Erro interno")})
-    public Response post(BusinessDto dto) {
+    public Response business() {
         try {
-            return service.save(dto);
+            return service.accounts();
         } catch (Exception e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
@@ -46,21 +44,73 @@ public class BusinessResource {
     }
 
     @GET
-    @Operation(summary = "Lista Negócios", description = "Lista Lojas ou Clínicas Veterinárias")
+    @Path(("{id}"))
+    @Operation(summary = "Negócio", description = "Busca Negócio por id")
     @APIResponses(value = {
             @APIResponse(responseCode = "200", description = "Sucesso",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = BusinessDto.class))),
-            @APIResponse(responseCode = "403", description = "Usuário não encontrado"),
+            @APIResponse(responseCode = "403", description = "Negócio não encontrada"),
             @APIResponse(responseCode = "500", description = "Erro interno")})
-    public Response get() {
+    public Response business(@PathParam("id") long id) {
         try {
-            return Response.ok(service.listAll()
-                    .stream()
-                    .map(BusinessDto::fromEntity)
-                    .collect(Collectors.toList())).build();
+            return service.accounts();
         } catch (Exception e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
         }
     }
+
+    @POST
+    @Operation(summary = "Criar", description = "Cria Negócio que pode ser uma Loja ou Clínica Veterinária")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "201", description = "Criado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = BusinessDto.class))),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response create(BusinessDto dto) {
+        try {
+            return service.create(dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
+    @PUT
+    @Path("{id}")
+    @Operation(summary = "Atualizar", description = "Atualiza Negócio")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = String.class))),
+            @APIResponse(responseCode = "403", description = "Negócio não encontrada"),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response update(@PathParam("id") long id, BusinessDto dto) {
+        try {
+            return service.update(id, dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Operation(summary = "Desativar", description = "Desativa Negócio")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "204", description = "Desativado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = String.class))),
+            @APIResponse(responseCode = "403", description = "Negócio não encontrado"),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response disable(@PathParam("id") long id) {
+        try {
+            return service.disable(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
 }
