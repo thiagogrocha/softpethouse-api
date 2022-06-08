7c80883b6de6ebcb2dd5f61be971f292ad34ef3c
TRochaDev <trochadev@gmail.com>
2022-06-07 22:20:49 -0300

    Atualiza Resources.java

--- src/main/java/br/com/softpethouse/api/Resources.java
+++ src/main/java/br/com/softpethouse/api/Resources.java
@@ -1,16 +1,16 @@
 package br.com.softpethouse.api;
 
-public interface Resources {
+public final class Resources {
 
-    String ACCOUNT = "account";
-    String TYPEACCOUNT = "typeaccount";
-    String BUSINESS = "business";
-    String ADOPTION = "adoption";
-    String NEGOTIATION = "negotiation";
-    String PET = "pet";
-    String HISTORIC = "historic";
-    String PRODUCT = "product";
-    String USER = "user";
-
+    public static final String VERSAO_API = "V0";
+    public static final String ACCOUNT = VERSAO_API + "/account";
+    public static final String TYPEACCOUNT = VERSAO_API + "/typeaccount";
+    public static final String BUSINESS = VERSAO_API + "/business";
+    public static final String ADOPTION = VERSAO_API + "/adoption";
+    public static final String NEGOTIATION = VERSAO_API + "/negotiation";
+    public static final String PET = VERSAO_API + "/pet";
+    public static final String HISTORIC = VERSAO_API + "/historic";
+    public static final String PRODUCT = VERSAO_API + "/product";
+    public static final String USER = VERSAO_API + "/user";
 
 }
