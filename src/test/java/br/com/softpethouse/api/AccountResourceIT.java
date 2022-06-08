0aec1503f500a1ac18b555b88187d827bb454218
TRochaDev <trochadev@gmail.com>
2022-06-07 22:12:20 -0300

    Renomeia Classe de test.

--- src/test/java/br/com/softpethouse/api/AccountResourceIT.java
+++ src/test/java/br/com/softpethouse/api/AccountResourceIT.java
@@ -3,7 +3,7 @@ package br.com.softpethouse.api;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class ExampleResourceIT extends ExampleResourceTest {
+public class AccountResourceIT extends AccountResourceTest {
 
     // Execute the same tests but in native mode.
 }
---
+++ src/test/java/br/com/softpethouse/api/AccountResourceTest.java
@@ -0,0 +1,25 @@
+package br.com.softpethouse.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+class AccountResourceTest {
+
+    @Test
+    void testAccountsByIdEndpoint() {
+        given()
+                .when().get(Resources.ACCOUNT)
+                .then()
+                .statusCode(200);
+//                .body(is(new ObjectMapper().writeValueAsString(AccountDtoOut.builder()
+//                        .id(12).active("S").username("")
+//                        .user(UserDto.builder().name("Thiago").age(32).build())
+//                        .typeAccount(TypeAccountDto.builder().name("ADM Negócio").description("description").build())
+//                        .business(BusinessDto.builder().name("Pet e Cia PetShop").description("PetShop").build()).build())));
+    }
+
+}
