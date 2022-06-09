06a4da32e5e972ef071ddd02a2be1bafe70cebaa
TRochaDev <trochadev@gmail.com>
2022-06-08 22:36:03 -0300

    Cria métodos de respostas.

--- src/main/java/br/com/softpethouse/api/account/resource/TypeAccountResource.java
+++ src/main/java/br/com/softpethouse/api/account/resource/TypeAccountResource.java
@@ -1,9 +1,8 @@
 package br.com.softpethouse.api.account.resource;
 
 import br.com.softpethouse.api.Resources;
+import br.com.softpethouse.api.account.dto.AccountDtoUpdate;
 import br.com.softpethouse.api.account.dto.TypeAccountDto;
-import br.com.softpethouse.api.account.entity.AccountEntity;
-import br.com.softpethouse.api.account.entity.TypeAccountEntity;
 import br.com.softpethouse.api.account.service.TypeAccountService;
 import br.com.softpethouse.api.commom.validation.ResponseError;
 import org.eclipse.microprofile.openapi.annotations.Operation;
@@ -13,10 +12,7 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -32,17 +28,85 @@ public class TypeAccountResource {
         this.service = service;
     }
 
+    @GET
+    @Operation(summary = "Tipos de Conta de Usuário", description = "Lista todos os tipos de Conta de Usuário")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = TypeAccountDto[].class))),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response typesAccount() {
+        try {
+            return service.typesAccount();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
+    @GET
+    @Path(("{id}"))
+    @Operation(summary = "Tipo de Conta de Usuário", description = "Busca Tipos de Conta de Usuário por id")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = TypeAccountDto.class))),
+            @APIResponse(responseCode = "403", description = "Tipo de Conta de Usuário não encontrada"),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response typesAccount(@PathParam("id") long id) {
+        try {
+            return service.typesAccount(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
     @POST
-    @Operation(summary = "Cria Tipo de Conta", description = "Um Conta pode ser do tipo Cliente, Consultor ou Administrador")
+    @Operation(summary = "Criar", description = "Cria um Tipo de Conta de Usuário que pode ser Cliente, Consultor ou Administrador")
     @APIResponses(value = {
             @APIResponse(responseCode = "201", description = "Criado",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = TypeAccountDto.class))),
             @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
             @APIResponse(responseCode = "500", description = "Erro interno")})
-    public Response post(TypeAccountDto dto) {
+    public Response create(TypeAccountDto dto) {
         try {
-            return service.save(dto);
+            return service.create(dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
+    @PUT
+    @Path("{id}")
+    @Operation(summary = "Atualizar", description = "Atualiza um Tipo de Conta de Usuário")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = String.class))),
+            @APIResponse(responseCode = "403", description = "Tipo de Conta não encontrada"),
+            @APIResponse(responseCode = "422", description = "Campos obrigatórios não informados",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ResponseError.class))),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response update(@PathParam("id") long id, TypeAccountDto dto) {
+        try {
+            return service.update(id, dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Operation(summary = "Desativar", description = "Desativa um Tipo de Conta de Usuário")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "204", description = "Desativado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = String.class))),
+            @APIResponse(responseCode = "403", description = "Conta não encontrada"),
+            @APIResponse(responseCode = "500", description = "Erro interno")})
+    public Response disable(@PathParam("id") long id) {
+        try {
+            return service.disable(id);
         } catch (Exception e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(e).build();
