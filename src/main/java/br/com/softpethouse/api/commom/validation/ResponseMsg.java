9126faf4e498c286e29fe32607766be1c38459d4
TRochaDev <trochadev@gmail.com>
2022-06-08 23:03:37 -0300

    Ajusta identação.

--- src/main/java/br/com/softpethouse/api/commom/validation/ResponseMsg.java
+++ src/main/java/br/com/softpethouse/api/commom/validation/ResponseMsg.java
@@ -6,9 +6,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class ResponseMsg {
+
     private String msg;
 
     public ResponseMsg(String msg) {
         this.msg = msg;
     }
+
 }
