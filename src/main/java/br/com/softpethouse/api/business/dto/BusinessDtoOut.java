7419104d1ca666e7b5ebce11fbcd861f47927de5
TRochaDev <trochadev@gmail.com>
2022-06-10 00:08:44 -0300

    Adiciona Jsonb como propertyOrder remove construtores.

--- src/main/java/br/com/softpethouse/api/business/dto/BusinessDtoOut.java
+++ src/main/java/br/com/softpethouse/api/business/dto/BusinessDtoOut.java
@@ -1,47 +1,30 @@
 package br.com.softpethouse.api.business.dto;
 
-import br.com.softpethouse.api.business.entity.BusinessEntity;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 
+import javax.json.bind.annotation.JsonbPropertyOrder;
+
 @Data
-@NoArgsConstructor
 @Schema(name = "Business", description = "Business dto out")
-@JsonPropertyOrder({"id", "name", "description", "active"})
+@JsonbPropertyOrder({"id", "active", "name", "description"})
 public class BusinessDtoOut {
 
     @Schema(title = "Id do Negócio")
     @Parameter(description = "Id parameter")
-    @JsonProperty(index = 1)
     private long id;
 
     @Schema(title = "Nome do Negócio")
     @Parameter(description = "Nome parameter")
-    @JsonProperty(index = 2)
     private String name;
 
     @Schema(title = "Descrição")
     @Parameter(description = "Descrição parameter")
-    @JsonProperty(index = 3)
     private String description;
 
     @Schema(title = "Ativo")
     @Parameter(description = "Ativo parameter")
-    @JsonProperty(index = 4)
     private String active;
 
-    public BusinessDtoOut(long id, String name, String description, String active) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-    }
-
-    public static BusinessDtoOut fromEntity(BusinessEntity entity) {
-        return new BusinessDtoOut(entity.getId(), entity.getName(), entity.getDescription(), entity.getActive());
-    }
 }
