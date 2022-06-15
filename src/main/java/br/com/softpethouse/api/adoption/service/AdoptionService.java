46c718a5d8e29465d4e4c84dfb823dde34e6face
TRochaDev <trochadev@gmail.com>
2022-06-14 23:58:53 -0300

    Adiciona SLof4j.

--- src/main/java/br/com/softpethouse/api/adoption/service/AdoptionService.java
+++ src/main/java/br/com/softpethouse/api/adoption/service/AdoptionService.java
@@ -1,10 +1,12 @@
 package br.com.softpethouse.api.adoption.service;
 
-import br.com.softpethouse.api.adoption.AdoptionEntity;
+import lombok.extern.slf4j.Slf4j;
+import br.com.softpethouse.api.adoption.entity.AdoptionEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 
+@Slf4j
 @ApplicationScoped
 public class AdoptionService implements PanacheRepository<AdoptionEntity> {
 }
