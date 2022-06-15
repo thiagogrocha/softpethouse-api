9261b721411747ae500004c646182b4bf3ee10ca
TRochaDev <trochadev@gmail.com>
2022-06-14 23:58:37 -0300

    Adiciona mapper.

--- src/main/java/br/com/softpethouse/api/user/service/UserService.java
+++ src/main/java/br/com/softpethouse/api/user/service/UserService.java
@@ -1,15 +1,22 @@
 package br.com.softpethouse.api.user.service;
 
+import br.com.softpethouse.api.user.dto.UserDto;
 import br.com.softpethouse.api.user.entity.UserEntity;
+import br.com.softpethouse.api.user.mapper.UserMapper;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class UserService implements PanacheRepository<UserEntity> {
 
-    public UserEntity create(String name, int age) {
-        UserEntity entity = new UserEntity(name, age);
+    @Inject
+    UserMapper mapper;
+
+    public UserEntity create(UserDto dto) {
+        UserEntity entity = mapper.toEntity(dto);
+
         persist(entity);
 
         return entity;
