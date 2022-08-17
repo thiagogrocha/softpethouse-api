5d3539af96b4b324f10b0c1da33d868d6fbcd5a4
TRochaDev <trochadev@gmail.com>
2022-08-17 00:11:39 -0300

    Ajusta NegotiationEntity .

--- src/main/java/br/com/softpethouse/api/negotiation/entity/NegotiationEntity.java
+++ src/main/java/br/com/softpethouse/api/negotiation/entity/NegotiationEntity.java
@@ -1,9 +1,10 @@
 package br.com.softpethouse.api.negotiation.entity;
 
-import br.com.softpethouse.api.negotiationItem.entity.NegotiationItemEntity;
+import br.com.softpethouse.api.business.entity.BusinessEntity;
+import br.com.softpethouse.api.negotiation.dto.NegotiationDtoCreate;
+import lombok.Builder;
 import lombok.Data;
 import br.com.softpethouse.api.commom.EntityBase;
-import br.com.softpethouse.api.product.entity.ProductEntity;
 import br.com.softpethouse.api.user.entity.UserEntity;
 
 import javax.persistence.*;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 @Entity
 @Table(name = "negotiations")
 @SequenceGenerator(name = "NegotiationsSeq", sequenceName = "seq_negotiations", allocationSize = 1)
@@ -24,9 +26,9 @@ public class NegotiationEntity extends EntityBase {
     @JoinColumn(name = "iduser")
     private UserEntity user;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "idnegotiation")
-    private List<NegotiationItemEntity> negotiationItems;
+    @ManyToOne
+    @JoinColumn(name = "idbusiness")
+    private BusinessEntity business;
 
     @Column(nullable = false)
     private double value;
@@ -34,10 +36,27 @@ public class NegotiationEntity extends EntityBase {
     @Column(nullable = false)
     private LocalDateTime dateTime;
 
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "idnegotiation")
+    private List<NegotiationItemEntity> negotiationItems;
+
     @PrePersist
     private void setDefaultDateTime() {
         if (dateTime == null)
             dateTime = LocalDateTime.now();
+    }
+
+    public static NegotiationEntity toEntity(NegotiationDtoCreate dto, UserEntity user, BusinessEntity business) {
+        return NegotiationEntity.builder()
+                .user(user)
+                .business(business)
+                .value(dto.getValue())
+                .description(dto.getDescription())
+                .negotiationItems()
+                .build();
     }
 
 }
