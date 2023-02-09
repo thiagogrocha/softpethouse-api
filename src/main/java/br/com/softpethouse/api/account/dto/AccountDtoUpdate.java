a471a016b68db335519db393837b2f9b17359690
TRochaDev <trochadev@gmail.com>
2023-02-08 21:16:01 -0300

    Atualiza AccountDtoUpdate.

--- src/main/java/br/com/softpethouse/api/account/dto/AccountDtoUpdate.java
+++ src/main/java/br/com/softpethouse/api/account/dto/AccountDtoUpdate.java
@@ -4,29 +4,14 @@ import lombok.Data;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Data
-@Schema(name = "Account", description = "Account dto to create")
+@Schema(name = "Account", description = "Account dto to update")
 public class AccountDtoUpdate {
 
-    @NotNull(message = "Campo IdTypeAccount é obrigatório!")
-    @Schema(title = "Id TypeAccount")
-    @Parameter(description = "IdTypeAccount parameter")
-    private long idTypeAccount;
-
-    @NotNull(message = "Campo IdBusiness é obrigatório!")
-    @Schema(title = "Id Business")
-    @Parameter(description = "IdBusiness parameter")
-    private long idBusiness;
-
-    @NotBlank(message = "Campo Nome de Usuário é obrigatório!")
     @Schema(title = "Username")
-    @Parameter(description = "Username parameter")
+    @Parameter(description = "UserName parameter")
     private String userName;
 
-    @NotBlank(message = "Campo password é obrigatório!")
     @Schema(title = "Password")
     @Parameter(description = "Password parameter")
     private String password;
