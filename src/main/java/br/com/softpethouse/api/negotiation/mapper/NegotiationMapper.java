b26b038753b7deb8c30a0867ec3036068bcbcb87
TRochaDev <trochadev@gmail.com>
2023-02-11 22:33:37 -0300

    Remove Mappers.

--- src/main/java/br/com/softpethouse/api/negotiation/mapper/NegotiationMapper.java
+++
@@ -1,7 +0,0 @@
-package br.com.softpethouse.api.negotiation.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "cdi")
-public interface NegotiationMapper {
-}
--- src/main/java/br/com/softpethouse/api/pet/mapper/PetMapper.java
+++
@@ -1,7 +0,0 @@
-package br.com.softpethouse.api.pet.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "cdi")
-public interface PetMapper {
-}
